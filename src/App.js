f16a1cefd41ec3cf0b7452b4aee78c0cb2d8e654
thamerblg <thamerbelgacem18@yahoo.fr>
2021-12-27 18:29:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const initialcount = 0;
+  const [count, setCount] = useState(initialcount);
+  const [pas, setPas] = useState(1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Simple app Counter with React & Hooks</h2>
+      <p
+        className="counterValue"
+        style={{ color: count === 0 ? "black" : count > 0 ? "green " : "red" }}
+      >
+        {count}
+      </p>
+      <div className="form-container">
+        <p>Modify the advancement step here: </p>
+        <form>
+          <input
+            className="jumbotron__window"
+            type="number"
+            value={pas}
+            onChange={(e) => setPas(parseInt(e.target.value))}
+            min="1"
+          />
+        </form>
+      </div>
+      <div className="btn-container">
+        <button className="" onClick={() => setCount(count - pas)}>
+          Decrease
+        </button>
+        <button className="" onClick={() => setCount(initialcount)}>
+          Reset
+        </button>
+        <button className="" onClick={() => setCount(count + pas)}>
+          Increase
+        </button>
+      </div>
     </div>
   );
 }
